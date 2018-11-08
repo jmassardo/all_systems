0fc74a4b8a87ceb932273bd4ae401206a8f0af5c
James Massardo <jmassardo@chef.io>
2018-11-08 14:20:53 -0600

    added windows audit stuffs back in

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,10 +5,5 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 include_recipe 'chef-client::default'
-case node['platform_family']
-when 'windows'
-  # Do nothing...
-else
-  include_recipe 'audit::default'
-end
+include_recipe 'audit::default'
 include_recipe 'cis_baseline::default'
