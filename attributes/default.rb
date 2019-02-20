e6b57c42873bc363fb9bdb479067c1c85c79124e
James Massardo <jmassardo@chef.io>
2019-02-20 10:04:26 -0600

    testing attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,28 +5,28 @@ default['chef_client']['splay'] = 60
 # Audit attributes
 default['audit']['reporter'] = 'chef-server-automate'
 default['audit']['fetcher'] = 'chef-server-automate'
-# node.default['audit']['attributes']['environment'] = node.chef_environment
-# default['audit']['profiles'] =  case node['platform_family']
-#                                 when 'windows'
-#                                   [
-#                                     {
-#                                       name: 'DevSec Windows Patch Baseline',
-#                                       compliance: 'admin/windows-patch-baseline',
-#                                     },
-#                                     {
-#                                       name: 'DevSec Windows Security Baseline',
-#                                       compliance: 'admin/windows-baseline',
-#                                     },
-#                                   ]
-#                                 else
-#                                   [
-#                                     {
-#                                       name: 'DevSec Linux Patch Benchmark',
-#                                       compliance: 'admin/linux-patch-baseline',
-#                                     },
-#                                     {
-#                                       name: 'DevSec Linux Security Baseline',
-#                                       compliance: 'admin/linux-baseline',
-#                                     },
-#                                   ]
-#                                 end
+default['audit']['attributes']['environment'] = node.chef_environment
+default['audit']['profiles'] =  case node['platform_family']
+                                when 'windows'
+                                  [
+                                    {
+                                      name: 'DevSec Windows Patch Baseline',
+                                      compliance: 'admin/windows-patch-baseline',
+                                    },
+                                    {
+                                      name: 'DevSec Windows Security Baseline',
+                                      compliance: 'admin/windows-baseline',
+                                    },
+                                  ]
+                                else
+                                  [
+                                    {
+                                      name: 'DevSec Linux Patch Benchmark',
+                                      compliance: 'admin/linux-patch-baseline',
+                                    },
+                                    {
+                                      name: 'DevSec Linux Security Baseline',
+                                      compliance: 'admin/linux-baseline',
+                                    },
+                                  ]
+                                end
