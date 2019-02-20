f91b3360ec942ca07af0f89580ab2e5d2ab7ad56
James Massardo <jmassardo@chef.io>
2019-02-20 09:48:54 -0600

    There's something burning in the kitchen...

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,32 +1,32 @@
 # Chef Client attributes
-node.default['chef_client']['interval'] = 300
-node.default['chef_client']['splay'] = 60
+default['chef_client']['interval'] = 300
+default['chef_client']['splay'] = 60
 
 # Audit attributes
-node.default['audit']['reporter'] = 'chef-server-automate'
-node.default['audit']['fetcher'] = 'chef-server-automate'
+default['audit']['reporter'] = 'chef-server-automate'
+default['audit']['fetcher'] = 'chef-server-automate'
 # node.default['audit']['attributes']['environment'] = node.chef_environment
-node.default['audit']['profiles'] = case node['platform_family']
-                                    when 'windows'
-                                      [
-                                        {
-                                          name: 'DevSec Windows Patch Baseline',
-                                          compliance: 'admin/windows-patch-baseline',
-                                        },
-                                        {
-                                          name: 'DevSec Windows Security Baseline',
-                                          compliance: 'admin/windows-baseline',
-                                        },
-                                      ]
-                                    else
-                                      [
-                                        {
-                                          name: 'DevSec Linux Patch Benchmark',
-                                          compliance: 'admin/linux-patch-baseline',
-                                        },
-                                        {
-                                          name: 'DevSec Linux Security Baseline',
-                                          compliance: 'admin/linux-baseline',
-                                        },
-                                      ]
-                                    end
+default['audit']['profiles'] =  case node['platform_family']
+                                when 'windows'
+                                  [
+                                    {
+                                      name: 'DevSec Windows Patch Baseline',
+                                      compliance: 'admin/windows-patch-baseline',
+                                    },
+                                    {
+                                      name: 'DevSec Windows Security Baseline',
+                                      compliance: 'admin/windows-baseline',
+                                    },
+                                  ]
+                                else
+                                  [
+                                    {
+                                      name: 'DevSec Linux Patch Benchmark',
+                                      compliance: 'admin/linux-patch-baseline',
+                                    },
+                                    {
+                                      name: 'DevSec Linux Security Baseline',
+                                      compliance: 'admin/linux-baseline',
+                                    },
+                                  ]
+                                end
