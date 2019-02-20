ad00cc505dae7f2aaaaa6b3a825ce49ebf3d26f3
James Massardo <jmassardo@chef.io>
2019-02-20 12:05:25 -0600

    attempting to pass env from chef to inspec

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,10 @@ default['audit']['reporter'] = 'chef-server-automate'
 default['audit']['fetcher'] = 'chef-server-automate'
 
 # push chef env over to inspec
-default['audit']['attributes']['environment'] = node.chef_environment
+node.normal['audit']['attributes']['environment'] = node.chef_environment
+# default['audit']['attributes'] = {
+#   environment: node.chef_environment,
+# }
 default['audit']['profiles'] =  case node['platform_family']
                                 when 'windows'
                                   [
